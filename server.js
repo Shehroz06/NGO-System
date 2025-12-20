156a21737b50a6501866a9ab4b580e49c90537a4
Shehroz06 <shehrozndm22@gmail.com>
2025-12-20 18:47:30 +0500

    Minor Bug fixes

--- server.js
+++ server.js
@@ -12,14 +12,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// -------- JWT middleware --------
 function requireAuth(req, res, next) {
   const h = req.headers.authorization || "";
   const token = h.startsWith("Bearer ") ? h.slice(7) : null;
   if (!token) return res.status(401).json({ message: "Missing token" });
 
   try {
-    req.user = jwt.verify(token, process.env.JWT_SECRET); // { user_id, role }
+    req.user = jwt.verify(token, process.env.JWT_SECRET); 
     next();
   } catch {
     return res.status(401).json({ message: "Invalid/expired token" });
